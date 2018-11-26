94255a7ab995d0b37002ff35af6149bd352c3e1c
w11w <qq379859838@gmail.com>
2018-11-26 21:11:39 +0800

    iiiiiiiiiii

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-//helloooooooooooooooooooooo
+//helliiiiiiiiiiiiiiiiiiiiiiii
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
